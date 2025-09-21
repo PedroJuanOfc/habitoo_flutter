4c047cc28f9520d373189ea00dce2c4df21a150a
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-09-21 07:49:18 -0300

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HabitooApp());
+}
+
+class HabitooApp extends StatelessWidget {
+  const HabitooApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
